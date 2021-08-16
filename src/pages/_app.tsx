6c672e3f51c808d53171c8ce3f2dc1628932abea
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-08-16 19:58:46 -0300

    refac home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { theme } from "../styles/Theme";
+import { theme } from "../styles/theme";
 import type { AppProps } from "next/app";
 
 import { ChakraProvider } from "@chakra-ui/react";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,31 +7,75 @@ import {
   ListItem,
   Text,
   UnorderedList,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { Swiper } from "../components/Swiper/Swiper";
 
 export default function Home() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <>
       <Head>
         <title>Worldtrip | Home</title>
       </Head>
-      <Flex direction="column" align="center" justify="center">
-        <Box as="header" padding={4}>
-          <Image src="/logo.svg" alt="World trip logo" />
+      <Flex direction="column">
+        <Box as="header" padding={4} alignSelf="center">
+          <Image
+            width={["81px", "81px", "184.06px"]}
+            height={["20px", "20px", "45.92px"]}
+            src="/logo.svg"
+            alt="World trip logo"
+          />
         </Box>
         <Box
           as="section"
-          backgroundImage="url(/background.png)"
+          backgroundImage="url(/banner.png)"
           backgroundRepeat="no-repeat"
+          width="100vw"
+          height={["163px", "163px", "335px"]}
           padding={4}
         >
-          <Text as="h2" color="white.50" fontSize="20px">
-            5 Continentes, infinitas possibilidades.
-          </Text>
-          <Text as="p" color="gray.50" fontSize="14px">
-            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-          </Text>
+          <Box
+            marginTop={["0", "0", "5%"]}
+            marginLeft={["2%", "8%"]}
+            width={["350px", "350px", "470px"]}
+          >
+            <Text
+              fontSize={["20px", "20px", "36px"]}
+              fontWeight="500"
+              color="white.50"
+            >
+              5 Continentes,
+            </Text>
+            <Text
+              fontSize={["20px", "20px", "36px"]}
+              fontWeight="500"
+              color="white.50"
+            >
+              infinitas possibilidades.
+            </Text>
+            <Text
+              as="p"
+              color="gray.50"
+              marginTop={3}
+              fontSize={["14px", "14px", "20px"]}
+            >
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
+            {isWideVersion && (
+              <Image
+                right="100"
+                top="150"
+                position="absolute"
+                src="/Airplane.png"
+                alt="airplane"
+              />
+            )}
+          </Box>
         </Box>
         <Box as="section">
           <UnorderedList padding="8px 0" color="gray.100" fontWeight={500}>
@@ -69,7 +113,7 @@ export default function Home() {
             <Text>Então escolha seu continente</Text>
           </Box>
         </Box>
-        <Box as="section" width="100%">
+        <Box as="section" marginTop="16px" width={["100%", "50%"]}>
           <Swiper />
         </Box>
       </Flex>
