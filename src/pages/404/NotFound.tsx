4e20b3112efd3b6907f70ec55c2c4753d739d29d
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-12-14 15:28:41 -0300

    feat: implementing not found page

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,3 +1,38 @@
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+
 export function NotFound() {
-  return <h1>Oops, not found! Sorry...</h1>;
+  return (
+    <>
+      <Flex
+        padding="1rem 0"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        bgColor="gray.50"
+        h="100vh"
+      >
+        <Image width="50%" src="/images/workers.jpg" />
+
+        <Box
+          position="absolute"
+          top="20%"
+          left="50%"
+          transform="translate(-50%,-70%)"
+        >
+          <Text as="h1" fontSize="26px" fontWeight="bold">
+            Desculpe... Página em construção!
+          </Text>
+          <Text as="p" width="500px">
+            Estamos trabalhando para proporcionar uma melhor experiência.
+            Pedimos desculpas pelo transtorno e agradecemos a compreensão!
+          </Text>
+        </Box>
+
+        <Button as="a" href="/" colorScheme="blue">
+          Voltar
+        </Button>
+      </Flex>
+    </>
+  );
 }
