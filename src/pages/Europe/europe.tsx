53c0b0acebb6cc748b4911cf6bad621acd3ce1bc
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-12-14 13:45:55 -0300

    added assets icons

--- src/pages/Europe/europe.tsx
+++ src/pages/Europe/europe.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Banner } from "../../components/Banner/Banner";
 import { CityCard } from "../../components/CityCard/CityCard";
 import { Header } from "../../components/Header/Header";
