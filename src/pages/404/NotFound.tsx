5852f64a61016b90206cb935051abfeb7a29c3c0
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-12-14 15:45:44 -0300

    final ajusts

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -7,28 +7,32 @@ export function NotFound() {
         direction="column"
         justifyContent="center"
         alignItems="center"
-        textAlign="center"
+        textAlign="justify"
         bgColor="gray.50"
         h="100vh"
       >
-        <Image width="50%" marginTop="100px" src="/images/workers.jpg" />
+        <Image
+          width={["85%", "50%"]}
+          marginTop="100px"
+          src="/images/workers.jpg"
+        />
 
         <Box
           position="absolute"
-          top="20%"
+          top={["30%", "20%"]}
           left="50%"
           transform="translate(-50%,-70%)"
         >
           <Text as="h1" fontSize="26px" fontWeight="bold">
             Desculpe... Página em construção!
           </Text>
-          <Text as="p" width="500px">
+          <Text as="p" width={{ lg: "500px" }}>
             Estamos trabalhando para proporcionar uma melhor experiência.
             Pedimos desculpas pelo transtorno e agradecemos a compreensão!
           </Text>
         </Box>
 
-        <Button as="a" href="/" colorScheme="blue" top="-50px">
+        <Button as="a" href="/" colorScheme="blue" top={{ lg: "-50px" }}>
           Voltar
         </Button>
       </Flex>
